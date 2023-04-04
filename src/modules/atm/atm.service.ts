8d6b1db55858c4454572b42585c1574a1e7d9fba
devleandrodias <leandrodbdias@gmail.com>
2023-04-04 12:02:39 -0300

    feat: refactor withdraw method in single responsabilies

--- src/modules/atm/atm.service.ts
+++ src/modules/atm/atm.service.ts
@@ -22,7 +22,9 @@ export class ATMService {
     private readonly transactionService: TransactionService
   ) {}
 
-  async deposit({ amount }: IATMDepositInput): Promise<IATMDepositOutput> {
+  public async deposit({
+    amount,
+  }: IATMDepositInput): Promise<IATMDepositOutput> {
     await this.transactionService.createTransaction(
       ETransactionType.DEPOSIT,
       amount
@@ -33,16 +35,28 @@ export class ATMService {
     return { balance };
   }
 
-  async withdraw({ amount }: IATMWithdrawInput): Promise<IATMWithdrawOutput> {
+  public async extract(): Promise<IATMExtractOutput> {
+    return {
+      balance: await this.transactionService.getBalance(),
+      transactions: await this.transactionService.getTransactions(),
+    };
+  }
+
+  private async getAvailableATMNotes(): Promise<
+    Array<{ note: number; quantity: number }>
+  > {
     const bankNotes = await this.atmNoteRepository.getAvailableATMNotes();
+    return bankNotes.sort((a, b) => b.note - a.note);
+  }
 
+  private calculateNotes(
+    bankNotes: Array<{ note: number; quantity: number }>,
+    amount: number
+  ): Array<{ note: number; quantity: number }> {
     const notes: { note: number; quantity: number }[] = [];
-
     let remainingValue = amount;
 
-    const bankNotesSorted = bankNotes.sort((a, b) => b.note - a.note);
-
-    for (const bankNote of bankNotesSorted) {
+    for (const bankNote of bankNotes) {
       if (bankNote.quantity <= 0) {
         continue;
       }
@@ -66,34 +80,52 @@ export class ATMService {
       }
     }
 
-    const totalNotes = notes.reduce(
-      (acc, note) => acc + note.note * note.quantity,
-      0
-    );
+    return notes;
+  }
 
+  private checkAvailableNotes(totalNotes: number, amount: number): void {
     if (totalNotes < amount) {
       throw new AppError(
         StatusCodes.UNPROCESSABLE_ENTITY,
         "O caixa eletrônico não possui notas suficientes para entregar o valor solicitado"
       );
     }
+  }
 
+  private async createWithdrawalTransaction(amount: number): Promise<void> {
     await this.transactionService.createTransaction(
       ETransactionType.WITHDRAWAL,
       amount
     );
+  }
 
+  private async debitNotes(
+    notes: Array<{ note: number; quantity: number }>
+  ): Promise<void> {
     for (const note of notes) {
       await this.atmNoteRepository.debitNoteQuantity(note.note, note.quantity);
     }
-
-    return { notes };
   }
 
-  async extract(): Promise<IATMExtractOutput> {
-    return {
-      balance: await this.transactionService.getBalance(),
-      transactions: await this.transactionService.getTransactions(),
-    };
+  public async withdraw({
+    amount,
+  }: IATMWithdrawInput): Promise<IATMWithdrawOutput> {
+    const bankNotes = await this.getAvailableATMNotes();
+
+    const notes = this.calculateNotes(bankNotes, amount);
+
+    let totalNotes = 0;
+
+    for (const note of notes) {
+      totalNotes += note.note * note.quantity;
+    }
+
+    this.checkAvailableNotes(totalNotes, amount);
+
+    await this.createWithdrawalTransaction(amount);
+
+    await this.debitNotes(notes);
+
+    return { notes };
   }
 }
