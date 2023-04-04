12cc31bd993f1eb8a7b319696a90dd671535b4fe
devleandrodias <leandrodbdias@gmail.com>
2023-04-04 13:38:25 -0300

    feat: updating knexfile and build script

--- knexfile.ts
+++ knexfile.ts
@@ -5,17 +5,20 @@ import "dotenv/config";
  */
 module.exports = {
   development: {
-    client: "sqlite3",
-    useNullAsDefault: true,
+    client: "postgresql",
+    connection: {
+      port: process.env.DATABASE_PORT,
+      user: process.env.DATABASE_USER,
+      host: process.env.DATABASE_HOST,
+      database: process.env.DATABASE_DATABASE,
+      password: process.env.DATABASE_PASSWORD,
+    },
     seeds: {
       directory: "./src/shared/infra/knex/seeds",
     },
     migrations: {
       tableName: "knex_migrations",
       directory: "./src/shared/infra/knex/migrations",
-    },
-    connection: {
-      filename: "./database/db.sqlite",
     },
   },
 
